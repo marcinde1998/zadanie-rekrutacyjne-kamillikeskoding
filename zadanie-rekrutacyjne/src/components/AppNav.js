709b64cd800a02b9bf2960dd01bd0ee397962040
unknown <marcinde1998@gmail.com>
2023-05-05 21:31:12 +0200

    portfolio klikane menu

--- zadanie-rekrutacyjne/src/components/AppNav.js
+++ zadanie-rekrutacyjne/src/components/AppNav.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const AppNav = () => {
+    const [isHovering, setIsHovering] = useState(false);
 
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
+
+    const [menuModalVisible, setmenuModalVisible] = useState(false);
+
+    const handleClickOn = () => {
+        setmenuModalVisible(true);
+    };
+
+    const handleClickOut = () => {
+        setmenuModalVisible(false);
+    };
 
     return (
 
         <nav className="mainNav">
+
+
             <div className="navBarPortfolio">
                 <ul>
                     <li>
@@ -17,23 +38,67 @@ const AppNav = () => {
             </div>
             <div className="navBar">
                 <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                </ul>
-                
-                <ul>
-                    <li>
-                        <Link to="/services">Services</Link>
-                    </li>
-                </ul>
-                
-                <ul>
-                    <li>
-                        <Link to="/portfolio">Portfolio</Link>
-                    </li>
-                </ul>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li>
+                            <Link to="/services">Services</Link>
+                            <div>
+                                <div>
+                                    <div
+                                        onMouseOver={handleMouseOver}
+                                        onMouseOut={handleMouseOut}
+                                    >
+                                        <span className="dot"><i className="arrow down"></i></span>
+                                    </div>
+
+                                    {isHovering && (
+                                        <div className="fixedServices">
+                                            <h2>Only visible when hovering div</h2>
+                                            <h2>bobbyhadz.com</h2>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li>
+                            <Link to="/portfolio">Portfolio</Link>
+                            <div>
+                                <span className="dot" onClick={() => {
+                                    if (menuModalVisible === true) {
+                                        setmenuModalVisible(false);
+                                    } else {
+                                        setmenuModalVisible(true);
+                                    }
+
+                                }}><i className="arrow down"></i></span>
+                            </div>
+                        </li>
+                    </ul>
+                    {menuModalVisible && (<div>
+                        <div className="fixedPortfolio">
+                            <div className="box">
+                                <div className="boxtext1">Ecomence shop for Printing House</div>
+                                <div className="boxtext2">ecoprint</div>
+                                <Link>Show more <span>&#129122;</span></Link>
+                            </div>
+                            <div className="box"> 
+                                <div className="boxtext1">Website for Software House</div>
+                                <div className="boxtext2">likescoding</div>
+                                <Link>Show more <span>&#129122;</span></Link>
+                            </div>
+                            <Link className="boxtext3">View all <span>&#129122;</span></Link>
+                        </div>
+                    </div>)}
+
+
                 </div>
                 {/* <div className="contactUsButton">
                 <ul>
